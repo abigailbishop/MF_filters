0649aa52fe5f850bb843b82918e7b24491f1880d
Abigail Bishop <ms.abigailbishop@gmail.com>
2024-03-19 11:46:45 -0500

    Update lines in dagman to from Myoungchul's paths to my paths

--- scripts/batch_run/wipac/ARA_dag_launch.sh
+++ scripts/batch_run/wipac/ARA_dag_launch.sh
@@ -1,3 +1,3 @@
-mkdir logs
+# mkdir logs
 condor_submit_dag -config dagman_config A2.dag
 condor_submit_dag -config dagman_config A3.dag
--- scripts/batch_run/wipac/ARA_job.sh
+++ scripts/batch_run/wipac/ARA_job.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # load in variables
-key=cw_band
+key=sub_info
 station=$1
 run=$2
-blind=0
+blind_dat=1
 condor_run=0
 not_override=0
 qual_type=1
 l2_data=0
-no_tqdm=1
+no_tqdm=0
 
 # run the reconstruction script
 export HDF5_USE_FILE_LOCKING='FALSE'
 source /cvmfs/ara.opensciencegrid.org/trunk/centos7/setup.sh
-source /home/mkim/analysis/MF_filters/setup.sh
-cd /home/mkim/analysis/MF_filters/scripts/
+source /home/abishop/ara/a23/MF_filters/setup.sh
+cd /home/abishop/ara/a23/MF_filters/scripts/
 
-python3 /home/mkim/analysis/MF_filters/scripts/script_executor.py -k ${key} -s ${station} -r ${run} -b ${blind} -c ${condor_run} -n ${not_override} -q ${qual_type} -t ${no_tqdm} -l ${l2_data}
+python3 /home/abishop/ara/a23/MF_filters/scripts/script_executor.py -k ${key} -s ${station} -r ${run} -b ${blind_dat} -c ${condor_run} -n ${not_override} -q ${qual_type} -t ${no_tqdm} -l ${l2_data}
 
