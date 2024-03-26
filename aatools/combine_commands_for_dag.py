58f7eac208617b6367d1afd3de908a716f0182bc
Abigail Bishop <ms.abigailbishop@gmail.com>
2024-03-26 12:09:28 -0500

    condor bills you for every job you queue and each run was analyzed in separate jobs so this script combines multiple runs into one job to save on priority

---
+++ aatools/combine_commands_for_dag.py
@@ -0,0 +1,94 @@
+def make_shell_script(
+    file_name, key, station, runs, 
+    blind_dat=1, condor_run=0, not_override=0, qual_type=1,
+    l2_data=0, no_tqdm=0, include_qual_cut=True
+): 
+
+    shebang = "#!/bin/bash"
+
+    build_environment = [
+        "# run the reconstruction script",
+        "export HDF5_USE_FILE_LOCKING='FALSE'",
+        "source /cvmfs/ara.opensciencegrid.org/trunk/centos7/setup.sh",
+        "source /home/abishop/ara/a23/MF_filters/setup.sh",
+        "cd /home/abishop/ara/a23/MF_filters/scripts/",
+    ]
+
+    debugging_statements = [
+        r'printf "\n"',
+        'pwd',
+        r'printf "\n"',
+    ]
+    
+    commands = []
+    for run in runs: 
+        commands.append(
+            f"python3 /home/abishop/ara/a23/MF_filters/scripts/script_executor.py "
+            f"-k {key} -s {station} -r {run} -b {blind_dat} "
+            f"-c {condor_run} -n {not_override} -q {qual_type} "
+            f"-t {no_tqdm} -l {l2_data} -qc {include_qual_cut} "
+        )
+
+    file = open(file_name, "w")
+    file.write(shebang+"\n\n")
+    for block in [build_environment, debugging_statements, commands]:
+        for line in block: 
+            file.write(line+"\n") 
+        file.write("\n\n")
+    file.close()
+
+    return None
+
+def main():
+
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('runlist', type=str,
+        help="Absolute path to runlist")
+    parser.add_argument('output_dir', type=str,
+        help="Absolute path to the directory where new shell scripts will be saved.")
+    parser.add_argument('station', type=int,
+        help="Number of the ARA station")
+    parser.add_argument('key', type=str,
+        help="key corresponding to the script called by script_executor.py")
+    parser.add_argument('--first_run', type=int, default = 0,
+        help="First run being analyzed")
+    parser.add_argument('--last_run', type=int, default = 100000,
+        help="Last run being analyzed")
+    parser.add_argument('--runs_per_job', type=int, default = 10,
+        help="Number of runs to execute the script for per job.")
+    parser.add_argument('--blind_dat', type=int, default=1)
+    parser.add_argument('--condor_run', type=int, default=0)
+    parser.add_argument('--not_override', type=int, default=0)
+    parser.add_argument('--qual_type', type=int, default=1)
+    parser.add_argument('--l2_data', type=int, default=0)
+    parser.add_argument('--no_tqdm', type=int, default=0)
+    parser.add_argument('--include_qual_cut', type=bool, default=True)
+
+    args = parser.parse_args()
+
+    from find_missing_outputs import get_runs_from_runlist
+
+    input_runs = get_runs_from_runlist(
+        args.runlist, args.first_run, args.last_run)
+    
+    split_runs = [
+        input_runs[ i*args.runs_per_job : (i+1)*args.runs_per_job ]
+        if (i*args.runs_per_job) < len(input_runs)
+        else input_runs[ i*args.runs_per_job : ]
+        for i in range( (len(input_runs)//args.runs_per_job) + 1  )
+    ]
+    print(f"Making {len(split_runs)} shells scripts for {len(input_runs)} jobs.")
+
+    for i, runs in enumerate(split_runs): 
+        make_shell_script(
+            f"{args.output_dir}/job{i}.sh", args.key, args.station, runs,
+            blind_dat=args.blind_dat, condor_run=args.condor_run, 
+            not_override=args.not_override, qual_type=args.qual_type,
+            l2_data=args.l2_data, no_tqdm=args.no_tqdm, 
+            include_qual_cut=args.include_qual_cut
+        )
+
+
+if __name__=="__main__":
+    main()
